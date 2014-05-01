abbbc5a10536cb9acd3c4630420322432156ce38
Robotnik00 <jritchey123@gmail.com>
2014-04-30 23:43:57 -0400

    added example

---
+++ example/DebugState.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <States.h>
+#include <Model.h>
+
+/// a simple debug state
+class DebugState : public Engine3d::State
+{
+public:
+    DebugState(Engine3d::Engine* engine);
+    ~DebugState();
+    void Update();
+    void Draw(float delta);
+    void ProcessEvent(SDL_Event* event);
+
+protected:
+
+    long mCounter;
+
+    Engine3d::Model* mModel1;
+    Engine3d::Model* mModel2;
+
+    glm::mat4 mTransform;
+
+    bool mKeysDown[256];
+
+};
---
+++ example/example.cpp
@@ -0,0 +1,25 @@
+#include <Engine.h>
+#include <States.h>
+#include <DebugState.h>
+#include <iostream>
+
+void callback()
+{
+	std::cout << "callback\n";
+    std::cout.flush();
+}
+
+int main(void)
+{
+    Engine3d::Engine* engine = new Engine3d::Engine(500, 500);
+    engine->SetInterval(callback, 1000);
+    //engine->RemoveInterval(callback);
+    Engine3d::State* state = new DebugState(engine);
+    engine->ChangeState(state);
+    engine->Start();
+    delete engine;
+
+
+	return 0;
+}
+
